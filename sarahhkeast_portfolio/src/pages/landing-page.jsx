ef76be32200599e2ebd0e51a47a1573cf8655b5e
Joel Cheverie <joelcheverie@hotmail.com>
2020-07-09 10:56:33 -0400

    Added contact assets and began setting up the contact and questions section. Scroll header needs work

--- sarahhkeast_portfolio/src/pages/landing-page.jsx
+++ sarahhkeast_portfolio/src/pages/landing-page.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
-import HeroFlowers from '../assets/hero_flowers.png'
+import HeroFlowers from '../assets/hero_flowers.png';
+import Mail from '../assets/mail.svg';
+import LinkedIn from '../assets/linkedin.svg';
+import Dribbble from '../assets/dribbble.svg';
 
 const landingPage = 'landingPage';
 
 class LandingPage extends Component {
 
+    state = {
+        isTop: true
+    }
+
+    handleScroll = (event) => {
+        console.log("Yo");
+        const { isTop } = this.state;
+        console.log(isTop);
+        console.log(window.scrollY);
+        if (window.scrollY === 0 && !isTop) {
+            this.setState({ isTop: true });
+        } else if (window.scrollY !== 0 && isTop) {
+            this.setState({ isTop: false });
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ isTop: window.pageYOffset === 0 })
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
     render() {
+        const { isTop } = this.state;
         return (
             <div className={`${landingPage}`}>
-                <div className={`${landingPage}__header`}>
+                <div className={classnames(`${landingPage}__header`, !isTop ? `${landingPage}__header--white` : '')} id='header'>
                     <span className={`${landingPage}__header-name`}>Sarah Keast</span>
                     <span className={`${landingPage}__header-about`}>About</span>
                 </div>
@@ -25,7 +55,40 @@ class LandingPage extends Component {
                         <img className={`${landingPage}__image-hero`}
                             src={HeroFlowers} alt="Sarah Hero Flowers" />
                     </div>
+                </div>
+                <div className={`${landingPage}__question-contact-container`}>
+                    <div className={`${landingPage}__question-container`}>
+                        <h3 className={`${landingPage}__question-text`}>Any questions?</h3>
+                        <h3 className={`${landingPage}__question-text`}>Happy to help!</h3>
+                    </div>
+                    <div className={`${landingPage}__contact-container`}>
+                        <div className={`${landingPage}__contact-item`}>
+                            <img className={`${landingPage}__contact-image`}
+                                src={Mail}
+                                alt="mail-icon"></img>
+                            <span className={`${landingPage}__contact-text`}>sarah.keast1@gmail.com</span>
+                        </div>
+                        <div className={`${landingPage}__contact-item`}>
+                            <img className={`${landingPage}__contact-image`}
+                                src={LinkedIn}
+                                alt="linkedin-icon"></img>
+                            <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
+                        </div>
+                        <div className={`${landingPage}__contact-item`}>
+                            <img className={`${landingPage}__contact-image`}
+                                src={Dribbble}
+                                alt="dribbble-icon"></img>
+                            <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${landingPage}__footer`}>
+                    <div className={`${landingPage}__footer-wrapper`}>
+                        <span className={`${landingPage}__copyright-text`}>Sarah Keast 2020</span>
+                    </div>
+                    <div className={`${landingPage}__footer-spacer`}>
 
+                    </div>
                 </div>
             </div>
         )
