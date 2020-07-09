55b5041e8c80ea9e899b2d125c7fb7d267d6d230
Joel Cheverie <joelcheverie@hotmail.com>
2020-07-09 12:09:11 -0400

    Added a project card and got the alignment working properly

--- sarahhkeast_portfolio/src/pages/landing-page.jsx
+++ sarahhkeast_portfolio/src/pages/landing-page.jsx
@@ -5,6 +5,9 @@ import HeroFlowers from '../assets/hero_flowers.png';
 import Mail from '../assets/mail.svg';
 import LinkedIn from '../assets/linkedin.svg';
 import Dribbble from '../assets/dribbble.svg';
+import ArrowRightBlue from '../assets/arrow-right-blue.svg';
+import Yokko1 from '../assets/Yokko 1.png';
+import Yokko2 from '../assets/Yokko 2.png';
 
 const landingPage = 'landingPage';
 
@@ -56,29 +59,55 @@ class LandingPage extends Component {
                             src={HeroFlowers} alt="Sarah Hero Flowers" />
                     </div>
                 </div>
+                <div className={`${landingPage}__project-list-container`}>
+                    <div className={`${landingPage}__project-list-title-container`}>
+                        <h2 className={`${landingPage}__project-list-title`}>Work</h2>
+                        <div className={`${landingPage}__project-list-spacer`}></div>
+                    </div>
+                    <div className={`${landingPage}__project-card-container`}>
+                        <div className={`${landingPage}__project-text-container`}>
+                            <div className={`${landingPage}__project-title-container`}>
+                                <div className={`${landingPage}__project-title-align-container`}>
+                                    <h4 className={`${landingPage}__project-title`}>Yokko</h4>
+                                    <h4 className={`${landingPage}__project-sub-title`}>UX/UI</h4>
+                                </div>
+                                <img className={`${landingPage}__project-title-arrow`}
+                                    src={ArrowRightBlue}
+                                    alt='project-title-arrow' />
+                            </div>
+                        </div>
+                        <div className={`${landingPage}__project-image-container`}>
+                            <img className={`${landingPage}__project-image-one`} src={Yokko1} alt='project image one' />
+                            <img className={`${landingPage}__project-image-two`} src={Yokko2} alt='project image one' />
+                        </div>
+                    </div>
+
+                </div>
                 <div className={`${landingPage}__question-contact-container`}>
                     <div className={`${landingPage}__question-container`}>
                         <h3 className={`${landingPage}__question-text`}>Any questions?</h3>
                         <h3 className={`${landingPage}__question-text`}>Happy to help!</h3>
                     </div>
                     <div className={`${landingPage}__contact-container`}>
-                        <div className={`${landingPage}__contact-item`}>
-                            <img className={`${landingPage}__contact-image`}
-                                src={Mail}
-                                alt="mail-icon"></img>
-                            <span className={`${landingPage}__contact-text`}>sarah.keast1@gmail.com</span>
-                        </div>
-                        <div className={`${landingPage}__contact-item`}>
-                            <img className={`${landingPage}__contact-image`}
-                                src={LinkedIn}
-                                alt="linkedin-icon"></img>
-                            <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
-                        </div>
-                        <div className={`${landingPage}__contact-item`}>
-                            <img className={`${landingPage}__contact-image`}
-                                src={Dribbble}
-                                alt="dribbble-icon"></img>
-                            <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
+                        <div classname={`${landingPage}__contact-alignment-container`}>
+                            <div className={`${landingPage}__contact-item`}>
+                                <img className={`${landingPage}__contact-image`}
+                                    src={Mail}
+                                    alt="mail-icon"></img>
+                                <span className={`${landingPage}__contact-text`}>sarah.keast1@gmail.com</span>
+                            </div>
+                            <div className={`${landingPage}__contact-item`}>
+                                <img className={`${landingPage}__contact-image`}
+                                    src={LinkedIn}
+                                    alt="linkedin-icon"></img>
+                                <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
+                            </div>
+                            <div className={`${landingPage}__contact-item`}>
+                                <img className={`${landingPage}__contact-image`}
+                                    src={Dribbble}
+                                    alt="dribbble-icon"></img>
+                                <span className={`${landingPage}__contact-text`}>Sarah Keast</span>
+                            </div>
                         </div>
                     </div>
                 </div>
