b175b62b165c2fc66af21b03611ae895fe19fdc5
Joel Cheverie <joelcheverie@hotmail.com>
2020-07-16 20:35:48 -0400

    Added a mobile header with React Portal support

--- sarahhkeast_portfolio/src/components/header.jsx
+++ sarahhkeast_portfolio/src/components/header.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import classnames from 'classnames';
 
+import MenuMobile from '../assets/menu-mobile.svg';
+import Close from '../assets/close.svg';
+
+import MenuModal from './menu-modal';
+
 const header = 'header';
+const menuModal = 'menuModal';
 
 const Header = () => {
 
@@ -9,16 +15,22 @@ const Header = () => {
         document.body.getBoundingClientRect()
     );
 
+    const [showModal, setShowModal] = useState(false);
 
     const listener = e => {
         setBodyOffset(document.body.getBoundingClientRect());
     };
 
     useEffect(() => {
+        if (showModal) {
+            document.body.style.overflow = 'hidden';
+        }
         window.addEventListener("scroll", listener);
         return () => {
             window.removeEventListener("scroll", listener);
+            document.body.style.overflow = 'unset';
         };
+
     });
 
     const isAboutPage = () => {
@@ -38,6 +50,31 @@ const Header = () => {
                 }
             </div>
             <div className={`${header}__space-container`} />
+            <div className={`${header}__mobile-container`}>
+                <button className={classnames(showModal ? `${header}__mobile-menu-button--hide` : `${header}__mobile-menu-button`)} onClick={() => setShowModal(true)}><img src={MenuMobile} /></button>
+            </div>
+            {
+                showModal ?
+                    <MenuModal>
+                        <div className={`${menuModal}`}>
+                            <div className={`${menuModal}__overlay`} />
+                            <div className={`${menuModal}__content`}>
+                                <div className={`${menuModal}__close-button-container`}>
+                                    <button className={`${menuModal}__close-button`} onClick={() => setShowModal(false)}><img src={Close} /></button>
+                                </div>
+                                <div className={`${menuModal}__menu-option-container`}>
+                                    <a className={`${menuModal}__menu-option`} href='/'>
+                                        Home
+                                </a>
+                                    <a className={`${menuModal}__menu-option`} href='/about'>
+                                        About
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </MenuModal>
+                    : null
+            }
         </div>
     );
 };
