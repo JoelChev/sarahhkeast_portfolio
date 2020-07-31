cbca77c9cae96a22724c5a0dab841fafa08226fc
Joel Cheverie <joelcheverie@hotmail.com>
2020-07-30 20:39:07 -0400

    Added the assets and code for the illustration gallery page

--- sarahhkeast_portfolio/src/components/project-card.jsx
+++ sarahhkeast_portfolio/src/components/project-card.jsx
@@ -38,6 +38,10 @@ const ProjectCard = ({ id, title, subTitle, highLightedProject, leavingHighlight
                 <img className={classnames(`${projectCard}__image`,
                     images.align === 'center' ? `${projectCard}__image--center` : '',
                     images.align === 'right' ? `${projectCard}__image--right` : '')}
+                    style={{
+                        top: images.top ? images.top : '',
+                        left: images.left ? images.left : ''
+                    }}
                     src={require(`../assets/landing/${images.src}.png`)}
                     alt={`Project ${title} image`} />
                 <img className={`${projectCard}__image-mobile`}
--- sarahhkeast_portfolio/src/components/project-content.jsx
+++ sarahhkeast_portfolio/src/components/project-content.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 const projectContent = 'project-content';
 
-const ProjectContent = ({ type, title, text, images, highlightColor }) => {
+const ProjectContent = ({ type, title, text, textColor, images, highlightColor, backgroundColor }) => {
 
     const renderTextImageContent = () => {
         return (
@@ -76,6 +76,49 @@ const ProjectContent = ({ type, title, text, images, highlightColor }) => {
         )
     }
 
+    const renderScreenGalleryContent = () => {
+        return (
+            <React.Fragment>
+                <div className={`${projectContent}__screen-gallery-container`}
+                    style={{
+                        backgroundColor: backgroundColor ? backgroundColor : 'white'
+                    }}>
+                    <div className={`${projectContent}__screen-text-container`}>
+                        <h4 className={`${projectContent}__screen-title`}>
+                            {title}
+                        </h4>
+                        <span className={`${projectContent}__screen-text`}>
+                            {text}
+                        </span>
+                        <div className={`${projectContent}__images`}>
+                            {
+                                images.map((image, index) => {
+                                    return (
+                                        <React.Fragment>
+                                            <img key={`${title}-img-${index}`}
+                                                className={classnames(`${projectContent}__screen-image`, !image.src_mobile ? `${projectContent}__screen-image--show-mobile` : '')}
+                                                src={require(`../assets/${image.src}.png`)}
+                                                alt={image.alt_text} />
+                                            {
+                                                image.src_mobile ?
+                                                    <img key={`${title}-img-${index}`}
+                                                        className={`${projectContent}__screen-image-mobile`}
+                                                        src={require(`../assets/${image.src_mobile}.png`)}
+                                                        alt={image.alt_text} />
+                                                    : null
+                                            }
+
+                                        </React.Fragment>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+
     return (
         <div className={`${projectContent}`}>
             {
@@ -90,6 +133,12 @@ const ProjectContent = ({ type, title, text, images, highlightColor }) => {
                     :
                     null
             }
+            {
+                type === 'screen-gallery' ?
+                    renderScreenGalleryContent()
+                    :
+                    null
+            }
         </div>
     );
 
@@ -99,8 +148,10 @@ ProjectContent.propTypes = {
     type: PropTypes.string,
     title: PropTypes.string,
     text: PropTypes.string,
+    textColor: PropTypes.string,
     images: PropTypes.arrayOf(PropTypes.object),
     highlightColor: PropTypes.string,
+    backgroundColor: PropTypes.string,
 }
 
 export default memo(ProjectContent);
