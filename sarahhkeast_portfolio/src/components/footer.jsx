76ab5fc6766bc24aef413d804852ce22b2065c8d
Joel Cheverie <joelcheverie@hotmail.com>
2021-10-13 16:08:42 -0400

    Updated the footer date to 2022 (#2)

--- sarahhkeast_portfolio/src/components/footer.jsx
+++ sarahhkeast_portfolio/src/components/footer.jsx
@@ -1,36 +1,60 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 import PropTypes from "prop-types";
-import classnames from 'classnames';
+import classnames from "classnames";
 
-import { INSTAGRAM, LINKEDIN, DRIBBBLE } from '../constants/contact';
+import { INSTAGRAM, LINKEDIN, DRIBBBLE } from "../constants/contact";
 
-import Dribble from '../assets/dribbble.svg';
-import LinkedIn from '../assets/linkedin.svg';
-import Instagram from '../assets/instagram.svg';
+import Dribble from "../assets/dribbble.svg";
+import LinkedIn from "../assets/linkedin.svg";
+import Instagram from "../assets/instagram.svg";
 
-const footer = 'footer';
+const footer = "footer";
 
 const Footer = ({ isDark, showIcons }) => (
-
-    <div className={classnames(`${footer}`, isDark ? `${footer}--dark` : '')}>
-        <span className={`${footer}__copyright-text`}>Sarah Keast 2021</span>
-        {
-            showIcons ?
-                <div className={`${footer}__icon-container`}>
-                    <a className={`${footer}__icon-link`} href={DRIBBBLE} target="_blank" rel="noopener noreferrer"><img className={`${footer}__icon`} src={Dribble}></img></a>
-                    <a className={`${footer}__icon-link`} href={LINKEDIN} target="_blank" rel="noopener noreferrer"><img className={`${footer}__icon`} src={LinkedIn}></img></a>
-                    <a className={`${footer}__icon-link`} href={INSTAGRAM} target="_blank" rel="noopener noreferrer"><img className={classnames(`${footer}__icon`, `${footer}__icon--instagram`)} src={Instagram}></img></a>
-                </div>
-                :
-                <div className={`${footer}__spacer`} />
-        }
-
-    </div>
+  <div className={classnames(`${footer}`, isDark ? `${footer}--dark` : "")}>
+    <span className={`${footer}__copyright-text`}>Sarah Keast 2022</span>
+    {showIcons ? (
+      <div className={`${footer}__icon-container`}>
+        <a
+          className={`${footer}__icon-link`}
+          href={DRIBBBLE}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={`${footer}__icon`} src={Dribble}></img>
+        </a>
+        <a
+          className={`${footer}__icon-link`}
+          href={LINKEDIN}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className={`${footer}__icon`} src={LinkedIn}></img>
+        </a>
+        <a
+          className={`${footer}__icon-link`}
+          href={INSTAGRAM}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className={classnames(
+              `${footer}__icon`,
+              `${footer}__icon--instagram`
+            )}
+            src={Instagram}
+          ></img>
+        </a>
+      </div>
+    ) : (
+      <div className={`${footer}__spacer`} />
+    )}
+  </div>
 );
 
 Footer.propTypes = {
-    isDark: PropTypes.bool,
-    showIcons: PropTypes.bool,
-}
+  isDark: PropTypes.bool,
+  showIcons: PropTypes.bool,
+};
 
 export default memo(Footer);
